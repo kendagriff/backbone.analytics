b59ba5a9b43c559e138fcf48b7d97ff8a8ea5df6
Makis Tracend <makis.tracend@gmail.com>
2013-09-27 23:25:37 -0700

    fixing condition for defined _gaq

--- backbone.analytics.js
+++ backbone.analytics.js
@@ -1,12 +1,12 @@
 (function() {
   var loadUrl = Backbone.History.prototype.loadUrl;
-  
+
   Backbone.History.prototype.loadUrl = function(fragmentOverride) {
     var matched = loadUrl.apply(this, arguments),
         gaFragment = this.fragment;
     if (!/^\//.test(gaFragment)) gaFragment = '/' + gaFragment;
-    if(window._gaq !== undefined) window._gaq.push(['_trackPageview', gaFragment]);
-    
+    if(typeof window._gaq !== "undefined") window._gaq.push(['_trackPageview', gaFragment]);
+
     return matched;
   };
 
