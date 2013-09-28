4be43f4567270a99463770d791f2d303c5a69e67
Makis Tracend <makis.tracend@gmail.com>
2013-09-27 23:30:19 -0700

    supporting Analytics.js

--- backbone.analytics.js
+++ backbone.analytics.js
@@ -6,7 +6,10 @@
         gaFragment = this.fragment;
     if (!/^\//.test(gaFragment)) gaFragment = '/' + gaFragment;
     if(typeof window._gaq !== "undefined") window._gaq.push(['_trackPageview', gaFragment]);
-
+    if(typeof window['GoogleAnalyticsObject'] !== "undefined"){
+      var ga = window['GoogleAnalyticsObject'];
+      window[ga]('send', 'pageview', gaFragment);
+    }
     return matched;
   };
 
