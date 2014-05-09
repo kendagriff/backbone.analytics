6819f43dd0490b2c5e38ca825a9d2ac386671039
Walter Scarborough <wscarbor@tacc.utexas.edu>
2014-05-09 11:22:17 -0500

    Fixed JSHint errors

--- backbone.analytics.js
+++ backbone.analytics.js
@@ -1,4 +1,7 @@
 (function(factory) {
+
+  'use strict';
+
   if (typeof define === 'function' && define.amd) {
     define(['backbone'], factory);
   } else if (typeof exports === 'object') {
@@ -7,17 +10,27 @@
     factory(window.Backbone);
   }
 })(function(Backbone) {
+
+  'use strict';
+
   var loadUrl = Backbone.History.prototype.loadUrl;
 
   Backbone.History.prototype.loadUrl = function(fragmentOverride) {
     var matched = loadUrl.apply(this, arguments),
         gaFragment = this.fragment;
-    if (!/^\//.test(gaFragment)) gaFragment = '/' + gaFragment;
+
+    if (!/^\//.test(gaFragment)) {
+      gaFragment = '/' + gaFragment;
+    }
+
     // legacy version
-    if(typeof window._gaq !== "undefined") window._gaq.push(['_trackPageview', gaFragment]);
+    if (typeof window._gaq !== "undefined") {
+      window._gaq.push(['_trackPageview', gaFragment]);
+    }
+
     // Analytics.js
-    var ga = window['GoogleAnalyticsObject'] || window.ga;
-    if(typeof ga !== "undefined"){
+    var ga = window.GoogleAnalyticsObject || window.ga;
+    if (typeof ga !== 'undefined') {
       ga('send', 'pageview', gaFragment);
     }
     return matched;
